982eb92a7d04ab156558ef51d8670909379f4167
Marcin <lubera.marcin@gmail.com>
2018-03-25 18:52:46 +0200

    plik js

--- script.js
+++ script.js
@@ -20,25 +20,9 @@ function zmiejszCzcione()
     this.style.fontSize = "15px";
 }
 var test = document.getElementById ("test");
-/*
-test.onmouseover = zmienKolor;
-test.onmouseout = zmienKolor2;
-*/
 
 test.addEventListener("mouseover", zmienKolor);
 test.addEventListener("mouseout", zmienKolor2);
 test.addEventListener("mouseover", powiekszCzcionke);
 test.addEventListener("mouseout", zmiejszCzcione);
 
-function wykonaj (event)
-{
-    var test = document.getElementById("test");
-
-    test.innerHTML = event.clientX;
-}
-
-window.onload = function()
-
-var test = document.getElementById("test");
-
-test.onmousemove = wykonaj;
